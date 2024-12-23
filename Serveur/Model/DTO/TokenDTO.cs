3c39c6a65bbbae6efd2b63c4e8b5619db76be08d
MattisGaloppin <Mattis_Galoppin@etu.u-bourgogne.fr>
2024-12-23 13:48:59 +0100

    push dto

---
+++ Serveur/Model/DTO/TokenDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace Serveur.Model.DTO
+{
+    /// <summary>
+    /// Représente la table Tokens de la bd
+    /// </summary>
+    public class TokenDTO
+    {
+        /// <summary>
+        /// Permet d'obtenir l'id du token
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Permet d'obtenir ou de définir l'id du user
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Permet d'obtenir ou de définir un pwd
+        /// </summary>
+        public string Token  { get; set; }
+    }
+}
+}
---
+++ Serveur/Model/DTO/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace Serveur.Model.DTO
+{
+    /// <summary>
+    /// Represente la table user de la BD
+    /// </summary>
+    public class UserDTO
+    {
+        /// <summary>
+        /// Permet d'obtenir un id
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Permet d'obtenir ou de définir une Email
+        /// </summary>
+        public string Email { get; set; }
+        
+        /// <summary>
+        /// Permet d'obtenir ou de définir un pwd
+        /// </summary>
+        public string HashPassword { get; set; }
+    }
+}
